5d2fbaa90e22a54e15db7b9af6534b54f92602db
Lees <msle237.lees@gmail.com>
2023-07-20 16:10:23 -0400

    networking broken

--- src/camera_package/modules/regular/regular.py
+++ src/camera_package/modules/regular/regular.py
@@ -1,6 +1,38 @@
 import cv2
 from src.modules.networking import Networking
 
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 class Regular:
     def __init__(self, host, port, camera_id):
         self.host = host
@@ -22,6 +54,7 @@ class Regular:
         while True:
             # Capture a frame
             ret, frame = self.cam.read()
+            frame = image_resize(frame, width=480)
 
             if not ret:
                 print("Failed to capture frame")
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -2,6 +2,38 @@ import cv2
 import numpy as np
 from src.modules.networking import Networking
 
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 class WebcamClient:
     def __init__(self, host, port):
         self.host = host
@@ -16,6 +48,7 @@ class WebcamClient:
         while True:
             # Receive a frame
             frame = self.client_socket.recv_numpy()
+            frame = image_resize(frame, width=1920)
 
             # Display the frame
             cv2.imshow("Webcam", frame)
