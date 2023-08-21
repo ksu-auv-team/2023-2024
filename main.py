519131f063cc710cb844aedcc7a11af019d3c4a8
Lees <msle237.lees@gmail.com>
2023-08-21 16:29:09 -0400

    removed the templates directory (no longer needed) and started the main script

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+### Everything between these lines is for logging and system information  ###
+# _________________________________________________________________________ #
+
 # Import necessary libraries
 import os
 import sys
@@ -27,7 +30,7 @@ with open(config_file_path, mode="r", encoding="utf-8") as config_file:
 
 
 # Configure log file path
-def create_logger():
+def create_logger(filename="logfile"):
     """
     Creates and returns a logging object that writes log messages to a file.
     The filename includes a timestamp in the format "logfile_YYYY-MM-DD_HH-MM-SS.log".
@@ -38,7 +41,7 @@ def create_logger():
     logger_ = logging.getLogger(__name__)
     logger_.setLevel(logging.INFO)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_handler = logging.FileHandler(f"logs/logfile_{timestamp}.log")
+    file_handler = logging.FileHandler(f"logs/{filename}_{timestamp}.log")
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(formatter)
     logger_.addHandler(file_handler)
@@ -53,72 +56,62 @@ logger.info("Logger created")
 logger.info(f'Date: {datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
 logger.info(f"OS: {os_type}")
 logger.info(f"Python version: {sys.version}")
+logger.info(f"Python executable: {sys.executable}")
 
-# depending on config file [debug] flag, toggle debug flag
-if config["debug"]:
-    debug = True
-else:
-    debug = False
+# _________________________________________________________________________ #
 
-# Log debug flag
-logger.info(f"Debug: {debug}")
-
-# depending on config file [train] flag, toggle train flag
-if config["train"]:
-    train = True
-else:
-    train = False
-
-# Log train flag
-logger.info(f"Train: {train}")
-
-# Import necessary libraries
-import cv2
-import numpy as np
+### The following is a template comment box for use later ###
+# _________________________________________________________________________ #
 
 
-# Define regular camera class
-class RegCamera:
-    def __init__(self):
-        self.cam = cv2.VideoCapture(0)
-        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, config["camera"]["width"])
-        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, config["camera"]["height"])
-        self.cam.set(cv2.CAP_PROP_FPS, config["camera"]["fps"])
 
-    def get_frame(self):
-        _, frame = self.cam.read()
-        return frame
+# _________________________________________________________________________ #
 
-    def image_resize(self, image, width=None, height=None, inter=cv2.INTER_AREA):
-        # initialize the dimensions of the image to be resized and
-        # grab the image size
-        dim = None
-        (h, w) = image.shape[:2]
+### Regular Camera ### 
+# _________________________________________________________________________ #
 
-        # if both the width and height are None, then return the
-        # original image
-        if width is None and height is None:
-            return image
 
-        # check to see if the width is None
-        if width is None:
-            # calculate the ratio of the height and construct the
-            # dimensions
-            r = height / float(h)
-            dim = (int(w * r), height)
 
-        # otherwise, the height is None
-        else:
-            # calculate the ratio of the width and construct the
-            # dimensions
-            r = width / float(w)
-            dim = (width, int(h * r))
+# _________________________________________________________________________ #
 
-        # resize the image
-        resized = cv2.resize(image, dim, interpolation=inter)
+### ZED Camera and Depth Measurements ###
+# _________________________________________________________________________ #
 
-        # return the resized image
-        return resized
 
-    def release(self):
-        self.cam.release()
+
+# _________________________________________________________________________ #
+
+### Machine Vision ###
+# _________________________________________________________________________ #
+
+
+
+# _________________________________________________________________________ #
+
+### CNN's ###
+# _________________________________________________________________________ #
+
+
+
+# _________________________________________________________________________ #
+
+### Hardware Interface ###
+# _________________________________________________________________________ #
+
+
+
+# _________________________________________________________________________ #
+
+### Ground Control Station ###
+# _________________________________________________________________________ #
+
+
+
+# _________________________________________________________________________ #
+
+### Main Function ###
+# _________________________________________________________________________ #
+
+
+
+# _________________________________________________________________________ #
