3fd2ef65ba8ea90f726df695f27d8ccfc4a2a65a
Lees <msle237.lees@gmail.com>
2023-08-21 17:44:44 -0400

    added example opencv webcam two log files created

--- main.py
+++ main.py
@@ -69,7 +69,16 @@ logger.info(f"Python executable: {sys.executable}")
 
 ### Regular Camera ### 
 # _________________________________________________________________________ #
+import cv2
+import numpy as np
 
+cam = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cam.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 
 # _________________________________________________________________________ #
