7fad0323fd4910f38a3337c0c1466455e43ffabe
Jack Vega <47365403+CosmicError@users.noreply.github.com>
2023-10-31 15:15:06 -0400

    Task Cycles Added + Changes
     * Sim training & Real training have been removed in favor of a boolean logic check
     * Added different states for various stages of each of the 4 disclosed tasks
     * Added a bunch of different variables that record the stats each state during the task so if we ever pause it, it won't try and repeat steps
     * Now has the ability to record the start time of the autonomous mode
     * Now has the ability to record the time when the statemachine was started
     * Plans to make a more compact function bool check for returning manual control

--- modules/SM.py
+++ modules/SM.py
@@ -24,95 +24,178 @@ def get_control_bools() -> dict:
             }
 
 class Submarine(StateMachine):
-    idle = State(initial=True)
-    autonomous = State()
+    idle = State(initial=True) #Hover in place
+    autonomous = State() #
     manual = State()
-    sim_training = State()
-    real_training = State()
     shutdown = State(final=True)
     
-    search_for_target = State() # search for the target
-    target_found = State() # calculate the distance, angle, elevation, and other stats to get to the target
+    find_gate = State() #find the gate we want to go through
+    enter_gate = State() #go through the gate we previously targeted
+    follow_marker = State() #Follow the markers on the ground
     position_towards_target = State() # position itself towards the target
     move_to_target = State() # move to the target
-    execute_task = State() # has gotten the target
+    
+    #? task depends on what gate is choosen
+    #Task 1: [4 iterations per task assignment], (aka do this 4 times every time we are assigned to do this task)
+    find_bouy = State() #finds the correct bouy that has the desired marker (will have to be done 4 times)
+    touch_marker = State() #Touch the markers that match which side of that gate you choose
+    
+    #Task 2: [2 iterations per task assignment]
+    find_bin = State()# finds the bin that needs to be opened to drop a marker into it
+    open_bin = State()
+    drop_marker = State() #Drop a marker into the bin
+    
+    #Task 3: [2 iterations per task assignment]
+    find_stargate = State()
+    fire_torpedo = State() #fires a torpedo into a stargate, more points if the stargate is open than closed (make check to identify)
+
+    #Task 4: [X iterations per task assignment]
+    find_marker = State() #finds the marker that needs to be picked up
+    pickup_marker = State() #picks up the marker
+    check_bounds = State() #checks if it is within bound of the course (set octagon for task 4) #! State may be subject to removal
+    #?Repeat states from task 2
     
     start_submarine = (
         idle.to(autonomous, cond="as_enabled")
         | idle.to(manual, cond="mc_enabled")
-        | idle.to(sim_training, cond="sts_enabled")
-        | idle.to(real_training, cond="rts_enabled")
     )
     
     manual_control = manual.from_(
         autonomous, 
         idle, 
-        sim_training, 
-        real_training, 
-        search_for_target, 
-        target_found, 
-        position_towards_target, 
-        move_to_target, 
-        execute_task
+        follow_marker,
+        position_towards_target,
+        move_to_target,
+        find_bouy,
+        touch_marker,
+        find_marker,
+        pickup_marker,
+        check_bounds,
+        find_stargate,
+        fire_torpedo,
+        find_bin,
+        open_bin,
+        drop_marker,
     )
     
     return_manual_control = (
         manual.to(autonomous, cond="as_from_mc")
-        | manual.to(sim_training, cond="sts_from_mc")
-        | manual.to(real_training, cond="rts_from_mc")
-        | manual.to(search_for_target) # removed for now since idk about saving data
-        # | manual.to(target_found)
-        # | manual.to(position_towards_target)
-        # | manual.to(move_to_target)
-        # | manual.to(execute_task)
     )
     
-    mission_cycle = (
-        autonomous.to(search_for_target)
-        | sim_training.to(search_for_target)
-        | real_training.to(search_for_target)
-        | search_for_target.to(target_found, after="mission_cycle")
-        | target_found.to(position_towards_target, after="mission_cycle")
-        | position_towards_target.to(move_to_target, after="mission_cycle")
-        | move_to_target.to(execute_task, after="mission_cycle")
-        | execute_task.to(search_for_target)
+    gate_cycle(
+        autonomous.to(follow_marker)
+        | follow_marker.from_(
+            touch_marker,
+            drop_marker,
+            fire_torpedo,
+        )
+        | follow_marker.to(t1_cycle, cond="")
+        | follow_marker.to(t2_cycle, cond="")
+        | follow_marker.to(t3_cycle, cond="")
+        | follow_marker.to(t4_cycle, cond="")
+    )
+    
+    t1_cycle = (
+        find_bouy.to(position_towards_target) #find the target bouy
+        | position_towards_target.to(move_to_target) #position ourselves towards the targeted bouy
+        | move_to_target.to(touch_marker) #move towards the targeted bouy
+        | touch_marker.to(find_bouy) #touch the correct side of the bouy
+    )
+    
+    t2_cycle = (
+        find_bin.to(position_towards_target) #find our target bin
+        | position_towards_target.to(move_to_target) #position ourselves towards the targeted bin
+        | move_to_target.to(open_bin) #move towards our targeted bin
+        | open_bin.to(open_bin) #open the targeted bin
+        | drop_marker.to(find_bin) #drop the marker into the bin, if we have another iteration, then go back to finding the next bin
+    )
+    
+    t3_cycle = (
+        find_stargate.to(position_towards_target)
+        | position_towards_target.to(move_to_target)
+        | move_to_target.to(fire_torpedo)
+        | fire_torpedo.to(find_stargate)
+    )
+    
+    t4_cycle = (
+        find_bin.to(find_marker)
+        | position_towards_target.to(move_to_target)
+        | move_to_target.to(open_bin, cond="self.bin") #only start moving to open_bin we are on that step (will have to hammer out a more solid detection system later)
+        | open_bin.to(find_marker)
+        
+        | find_marker.to(position_towards_target)
+        | move_to_target.to(pickup_marker, cond="self.marker") #only start moving to pickup_marker we are on that step (will have to hammer out a more solid detection system later)
+        | pickup_marker.to(drop_marker)
+        | drop_marker.to(find_bin)
+        # | check_bounds.to() #! we need to always make sure we are in the octagon
     )
     
     set_idle = idle.from_(
         autonomous, 
         manual, 
-        sim_training, 
-        real_training, 
-        search_for_target, 
-        target_found, 
-        position_towards_target, 
-        move_to_target, 
-        execute_task
+        follow_marker,
+        position_towards_target,
+        move_to_target,
+        find_bouy,
+        touch_marker,
+        find_marker,
+        pickup_marker,
+        check_bounds,
+        find_stargate,
+        fire_torpedo,
+        find_bin,
+        open_bin,
+        drop_marker,
     )
     
     power_off = shutdown.from_(
         idle,
         autonomous, 
         manual, 
-        sim_training, 
-        real_training, 
-        search_for_target, 
-        target_found, 
-        position_towards_target, 
-        move_to_target, 
-        execute_task
+        follow_marker,
+        position_towards_target,
+        move_to_target,
+        find_bouy,
+        touch_marker,
+        find_marker,
+        pickup_marker,
+        check_bounds,
+        find_stargate,
+        fire_torpedo,
+        find_bin,
+        open_bin,
+        drop_marker,
     )
     
     def __init__(self, *args, **kwargs):
-        self.previous_state = ... # record out previous state
-        self.current_position = {"x":0, "y":0, "z":0, "alpha":0, "beta":0, "gama":0} #? https://en.wikipedia.org/wiki/Euler_angles
-        self.target_position_difference = {"x":0, "y":0, "z":0, "alpha":0, "beta":0, "gama":0}
-        self.target_position_list = list(self.target_position_difference.values())
+        self.previous_state: str = "" # records the previous state
+        self.time_start: float = 0.0 #records the time that the sub was turned on
+        self.auto_time_start: float = 0.0 #records the time when the sub first entered autonomous mode
+        
+        #? the robots targets (they might become strings instead of objects later)
+        self.gate: object = ... #this will hold what gate we want to go through #when we enter the gate, we want to set this to nothing or ...
+        self.marker: object = ... #the marker that we have targeted 
+        self.bin: object = ... #the bin we have targeted
+        self.stargate: object = ... #the stargate that we have targeted
+        
+        #? does the robot have it?
+        self.bin_opened: bool = False #whether the "self.bin_set" bin has been opened
+        self.marker_possesed: bool = False #if the robot has a marker in hand
+        
+        #? has the robot released it?
+        self.marker_dropped: bool = False #whether the marker in hand has been dropped #!(this variable may be subject a type to change)
+        self.torpedo_count: int = 2 #how many torpedos are currently loaded (will keep track of how many were fired)
+        
+        #? how many times have we done this?
+        self.t1_interation: int = 0
+        self.t2_interation: int = 0
+        self.t3_interation: int = 0
+        self.t4_interation: int = 0
+        
+        self.in_bounds: bool = False #whether we were in bounds last time we checked
+        
         super().__init__(*args, **kwargs)
    
-    
-    
-    
     # before we switch to manual mode, record out last state so we can go back to it using "return_manual_control"
     def before_manual_control(self, event: str, source: State, target: State):
         print("checking for controller")
@@ -225,9 +308,6 @@ class Submarine(StateMachine):
         print("when the sequence finishes, return to simulation training")
         return
     
-    
-    
-    
     def on_enter_state(self, target, event):
         print(f"{self.name} enter: {target.id} from {event}")
     
@@ -258,17 +338,14 @@ class Submarine(StateMachine):
             return True
         return False
         
-    def as_from_mc(self):
-        if self.previous_state == "autonomous":
-            return True
-        return False
-
-    def sts_from_mc(self):
-        if self.previous_state == "sim_training":
-            return True
+    def return_control(self):
+        #get the currently requestion state transition (as to mc, idle to mc, etc. as tell whether its most previous state was that)
+        #the idea is that if the previous state was move_to_target, then if the transition that is being requested is that, then approve.
+        #so we don't have 50 billion functions all doing the same thing slightly differently
+        
         return False
     
-    def rts_from_mc(self):
-        if self.previous_state == "real_training":
+    def as_from_mc(self):
+        if self.previous_state == "autonomous":
             return True
         return False
