ddc0e68378cd81222acddadf6d5b332cad0596e5
surgosan <sergy200118@gmail.com>
2024-02-16 17:54:35 -0500

    Basic Idea for Webcam Area

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,11 @@
 let webcam_on = false;
 let webcamStream;
 
-function test_webcam() {
+function test_webcam(camera_identifier) {
     if (!webcam_on) { //Start webcam
         navigator.mediaDevices.getUserMedia({ video: true })
             .then(function (stream) {
-                let webcam = document.getElementById('camera_feed_1');
+                let webcam = document.getElementById(`camera_feed_${camera_identifier}`);
                 webcam.srcObject = stream;
                 webcamStream = stream;
             })
@@ -14,8 +14,10 @@ function test_webcam() {
             });
         webcam_on = true;
     } else { //Turn off webcam
-        let webcam = document.getElementById('camera_feed_1');
+        let webcam = document.getElementById(`camera_feed_1`);
+        let webcam_2 = document.getElementById(`camera_feed_2`);
         webcam.srcObject = null;
+        webcam_2.srcObject = null;
         if (webcamStream) {
             let tracks = webcamStream.getTracks();
             tracks.forEach(function (track) {
