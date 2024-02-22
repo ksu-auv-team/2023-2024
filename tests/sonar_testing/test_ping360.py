98ba742429636d8037b86b67e0f2548fe337b506
Lees <msle237.lees@gmail.com>
2024-02-21 19:12:46 -0500

    testing sonar

--- tests/sonar_testing/test_ping360.py
+++ tests/sonar_testing/test_ping360.py
@@ -4,6 +4,10 @@ def test_ping360():
     p = Ping360()
     p.connect_serial("/dev/ttyUSB0", 115200)
 
+    print(p.set_transmit_frequency(800))
+    print(p.set_sample_period(80))
+    print(p.set_number_of_samples(200))
+
     # Get data
     while True:
         try:
