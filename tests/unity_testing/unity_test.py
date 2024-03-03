c3fba69d47a770f4dc3e1e04e169b37963872fe8
Lees <msle237.lees@gmail.com>
2024-03-02 22:41:48 -0500

    fixing unity

--- tests/unity_testing/unity_test.py
+++ tests/unity_testing/unity_test.py
@@ -199,7 +199,7 @@ class Unity:
         """
         @brief Send the data to the Unity simulation only works for 8 values. 
         """
-        s = f'{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]}R'
+        s = f'{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]},R'
         self.sock.sendall(s.encode('utf-8'))
     
     def run(self):
