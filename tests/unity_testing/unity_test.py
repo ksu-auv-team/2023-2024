c715ea30ef5d3f57390307e3235d2d8caca51627
Lees <msle237.lees@gmail.com>
2024-03-03 01:12:31 -0500

    fixing unity

--- tests/unity_testing/unity_test.py
+++ tests/unity_testing/unity_test.py
@@ -213,7 +213,7 @@ class Unity:
                 data = self.mapper.compute_movement(data)
                 self.send_data_8_values(data)
                 print(f'\r{data[0]}, {data[1]}, {data[2]}, {data[3]}, {data[4]}, {data[5]}, {data[6]}, {data[7]}', end='')
-                pygame.time.wait(10)
+                pygame.time.wait(11)
             except KeyboardInterrupt:
                 pygame.quit()
                 break
