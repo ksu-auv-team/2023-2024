2ef4ca2dcd8eaeca8c01a01a16c0d7687879c944
surgosan <sergy200118@gmail.com>
2024-03-28 02:14:05 -0400

    Slight Cleanup | Organized Chart js into single objects

---
+++ static/js/charts.js
@@ -0,0 +1,163 @@
+const intervalTitle = "6 Second Interval"
+const chart_colors = {
+    0: {color: '#0000FF'},
+    1: {color: '#FF0000'},
+    2: {color: '#FFFF00'},
+    3: {color: '#008000'},
+    4: {color: '#FF00FF'},
+    5: {color: '#00FFFF'},
+    6: {color: '#FFA500'},
+    7: {color: '#800080'}
+
+}
+let battery_voltage_chart = {}
+let battery_amp_chart = {}
+let motor_chart = {}
+let servo_chart = {}
+
+function initChartProperties() {
+    battery_voltage_chart = {
+        chart: 0,
+        chartData: null,
+        chartOptions: null,
+        subject: "Battery",
+        column_count: 4,
+        title: "Battery Voltage",
+        x_title: intervalTitle,
+        y_title: "Voltage",
+        y_max: 50,
+        container_id: 'battery_voltage',
+        unit_reference: batteries,
+        reference_unit: 'voltage',
+        selection_bool: false
+    };
+
+    battery_amp_chart = {
+        chart: 1,
+        chartData: null,
+        chartOptions: null,
+        subject: "Battery",
+        column_count: 4,
+        title: "Battery Amps",
+        x_title: intervalTitle,
+        y_title: "Amps",
+        y_max: 30,
+        container_id: 'battery_amp',
+        unit_reference: batteries,
+        reference_unit: 'amps',
+        selection_bool: false
+    };
+
+    motor_chart = {
+        chart: 2,
+        chartData: null,
+        chartOptions: null,
+        subject: "Motor",
+        column_count: 8,
+        title: "Motor PWM",
+        x_title: intervalTitle,
+        y_title: "PWM",
+        y_max: 100,
+        container_id: 'motor_pwm',
+        unit_reference: motors,
+        reference_unit: 'pwm',
+        selection_bool: false
+    };
+
+    servo_chart = {
+        chart: 3,
+        chartData: null,
+        chartOptions: null,
+        subject: 'Servo',
+        column_count: 2,
+        title: "Servo PWM",
+        x_title: intervalTitle,
+        y_title: "PWM",
+        y_max: 100,
+        container_id: 'servo_pwm',
+        unit_reference: servos,
+        reference_unit: 'pwm',
+        selection_bool: false
+    };
+}
+function data_charts(callback) {
+    initChart(battery_voltage_chart);
+    initChart(battery_amp_chart);
+    initChart(motor_chart);
+    initChart(servo_chart)
+
+    chartSelections(battery_voltage_chart);
+    chartSelections(battery_amp_chart);
+    chartSelections(motor_chart);
+    chartSelections(servo_chart);
+}
+
+function initChart(chart) {
+    const computedSize = window.getComputedStyle(document.getElementById('example_chart_size'));
+    chart.chartData = new google.visualization.DataTable();
+    for (let i = 0; i <= chart.column_count; i++) {
+        if(i === 0) { chart.chartData.addColumn('number', chart.y_title) } else { chart.chartData.addColumn('number', chart.subject + i); }
+    }
+    chart.chartOptions = {
+        backgroundColor: "#343434",
+        title: chart.title,
+        titleTextStyle: {color: "white"},
+        legend: {textStyle: {color: "#FFFFFF"}, position: 'in'},
+        hAxis: {title: intervalTitle,titleTextStyle: {color: "white"}, textStyle: {color: "white"}, baselineColor: "white", gridLines: {color: "#FFFFFF"}},
+        vAxis: {title: chart.y_title, titleTextStyle: {color: "white"}, textStyle: {color: "white"}, minValue: 0, maxValue: (chart.y_max+10)},
+        width: parseInt(computedSize.getPropertyValue('width')),
+        height: parseInt(computedSize.getPropertyValue('height')),
+        chartArea: {width: '70%', height: '85%', left: 70, right: 25},
+        explorer: {
+            actions: ['dragToZoom', 'rightClickToReset'],
+            axis: 'horizontal',
+            keepInBounds: true,
+            maxZoomIn: 10
+        },
+        series: chart_colors
+    }
+
+    chart.chart = new google.visualization.AreaChart(document.getElementById(chart.container_id));
+    chart.chart.draw(chart.chartData, chart.chartOptions);
+}
+function updateCharts(chart) { //Call this function per get/post request on .then
+    let newInsert = [timeActive];
+    chart.unit_reference.forEach(function(unit) {
+        newInsert.push(unit[chart.reference_unit]);
+    });
+    chart.chartData.addRow(newInsert);
+    chart.chart.draw(chart.chartData, chart.chartOptions);
+}
+
+function chartSelections(chart) {
+    google.visualization.events.addListener(chart.chart, 'click', function () {
+        let series = JSON.parse(JSON.stringify(chart_colors));
+        setTimeout(() => {
+            let selectedItem = chart.chart.getSelection();
+            if(!selectedItem) {
+                chart.chartOptions.series = series;
+                chart.chart.draw(chart.chartData, chart.chartOptions);
+                return;
+            }
+            if(!chart.selectionBool) {
+                if (selectedItem.length > 0) {
+                    let columnIndex = selectedItem[0].column - 1;
+                    if (columnIndex != null) {
+                        for (let i = 0; i < chart.chartData.getNumberOfColumns(); i++) {
+                            if (i !== columnIndex) {
+                                series[i] = {color: 'transparent'};
+                            }
+                        }
+                        chart.chartOptions.series = series;
+                        chart.chart.draw(chart.chartData, chart.chartOptions);
+                    }
+                    chart.selectionBool = true;
+                }
+            } else {
+                chart.chartOptions.series = series;
+                chart.chart.draw(chart.chartData, chart.chartOptions);
+                chart.selectionBool = false;
+            }
+        }, 40)
+    });
+}
--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     createBatteryElements();
     createMotorElements();
     createServoElements();
+    initChartProperties();
     startDataDemo();
-
 
     // //Google Chart API
     google.charts.load('current', {'packages':['corechart']});
@@ -16,60 +16,23 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 let data_demo
 let timeActive = 0;
-function timer() {
+function timer() { //later, call this after the api
     setInterval(() => {
         timeActive += 2;
-        updateBatteryVoltageChart();
-        updateBatteryAmpChart();
-        updateMotorChart();
-        updateServoChart();
-    }, 4000);
+        updateCharts(battery_voltage_chart);
+        updateCharts(battery_amp_chart);
+        updateCharts(motor_chart);
+        updateCharts(servo_chart)
+    }, 5500);
 }
 
 // ---------------------------------------- GLOBAL VARIABLES ----------------------------------------
-const numberOfBatteries = 4;
 const batteryMaxVoltage = 50;
 const batteryMaxAmps = 30;
 
-const numberOfMotors = 8;
 const motorMaxPWM = 100;
 
-
-
-// -------------------------------------------- TESTING WEBCAM   --------------------------------------------
-
-let webcam_on = false;
-let webcamStream;
-
-function test_webcam(camera_identifier) {
-    if (!webcam_on) { //Start webcam
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                let webcam = document.getElementById(`camera_feed_${camera_identifier}`);
-                webcam.srcObject = stream;
-                webcamStream = stream;
-            })
-            .catch(function (err) {
-                console.error('Unable to transmit camera feed: ', err);
-            });
-        webcam_on = true;
-    } else { //Turn off webcam
-        let webcam = document.getElementById(`camera_feed_1`);
-        let webcam_2 = document.getElementById(`camera_feed_2`);
-        webcam.srcObject = null;
-        webcam_2.srcObject = null;
-        if (webcamStream) {
-            let tracks = webcamStream.getTracks();
-            tracks.forEach(function (track) {
-                track.stop();
-            });
-        }
-        webcam_on = false;
-    }
-}
-
-//     -------------------------------------------- END TESTING WEBCAM  |  START TAB PAGES  --------------------------------------------
-
+//     -------------------------------------------- GLOBAL VARIABLES  |  START TAB PAGES  --------------------------------------------
 function switchTab(tab_index) {
     const stream = document.getElementById('stream_main');
     const data = document.getElementById('data_dialog');
@@ -96,66 +59,7 @@ function switchTab(tab_index) {
     }
 }
 
-
-//     -------------------------------------------- END TAB PAGES  |  START MESSAGE CENTER  --------------------------------------------
-
-function Message(severity, source, message) {
-    this.severity =  severity;
-    this.source= source;
-    this.message  = message;
-    this.toString = function () {
-        return `Severity: ${this.severity}  |  Source: ${this.source}  |  Message: ${this.message}\n`
-    }
-}
-
-let message_log = [];
-
-function newMessage(severity, source, message) {
-    let new_message = new Message(severity, source, message);
-    message_log.push(new_message);
-    displayMessage(new_message);
-    message_timeout();
-}
-
-function message_timeout() {
-    const message_center = document.getElementById('messages');
-
-    setTimeout(() => {
-        if (message_center.firstChild) {
-            message_center.firstElementChild.classList.add('single_message_clear');
-        }
-    }, 3000)
-
-    setTimeout(() => {
-        if (message_center.firstChild) {
-            message_center.firstChild.remove();
-        }
-    }, 4000)
-}
-
-function displayMessage(message_data) {
-    const message_container = document.createElement('div');
-    message_container.classList.add('single_message');
-
-    if(message_data.severity === 1) { message_container.classList.add('warning') }
-    if(message_data.severity >= 2) { message_container.classList.add('alert') }
-
-    const title = document.createElement("h3");
-    title.innerText = message_data.source;
-
-    const message_message = document.createElement("p");
-    message_message.innerText = message_data.message;
-
-    message_container.appendChild(title);
-    message_container.appendChild(message_message)
-
-    const message_center = document.getElementById('messages');
-    message_center.appendChild(message_container);
-}
-
-
-//     -------------------------------------------- END MESSAGE CENTER  |  START POWER BUTTON --------------------------------------------
-
+//     -------------------------------------------- GLOBAL VARIABLES  |  START POWER BUTTON --------------------------------------------
 let auv_power = false; //False = off
 
 function power() { //Make async when adding post requests
@@ -177,9 +81,6 @@ function power() { //Make async when adding post requests
 
 
 //     -------------------------------------------- END POWER BUTTON  |  START BATTERY DATA  --------------------------------------------
-
-
-
 const batteries = [
     { id: 1, voltage: 0, amps: 0 },
     { id: 2, voltage: 0, amps: 0 },
@@ -217,19 +118,7 @@ function createBatteryElements() {
         container.appendChild(batteryDiv);
     });
 }
-
-function resetBatteryElements() {
-    document.getElementById('battery_data').innerHTML = "";
-    createBatteryElements();
-}
-
-// Battery Google Area Chart
-function battery_chart() {
-
-}
-
 //     -------------------------------------------- END BATTERY DATA  |  START MOTOR DATA  --------------------------------------------
-
 const motors = [
     { id: 1, pwm: 0 },
     { id: 2, pwm: 0 },
@@ -262,13 +151,6 @@ function createMotorElements() {
         container.appendChild(batteryDiv);
     });
 }
-
-function resetMotorElements() {
-    document.getElementById('battery_data').innerHTML = "";
-    createMotorElements();
-}
-
-
 //     -------------------------------------------- END MOTOR DATA  |  START SERVO DATA  --------------------------------------------
 
 
@@ -298,244 +180,7 @@ function createServoElements() {
         container.appendChild(servo_div);
     });
 }
-
-function resetServoElements() {
-    document.getElementById('battery_data').innerHTML = "";
-    createBatteryElements();
-}
-
-
-//     -------------------- END SERVO DATA | START DATA CHARTS  --------------------
-const intervalTitle = "4 Second Interval"
-const chart_colors = {
-    0: {color: '#0000FF'},
-    1: {color: '#FF0000'},
-    2: {color: '#FFFF00'},
-    3: {color: '#008000'},
-    4: {color: '#FF00FF'},
-    5: {color: '#00FFFF'},
-    6: {color: '#FFA500'},
-    7: {color: '#800080'}
-}
-// BATTERY VOLTAGE ----------------------------------------------------------------------------------
-let batteryVoltageData;
-let batteryVoltageOptions;
-let batteryVoltageChart;
-function initBatteryVoltageChart() {
-    const computedSize = window.getComputedStyle(document.getElementById('example_chart_size'));
-    batteryVoltageData = google.visualization.arrayToDataTable([
-        ['3 Second Interval', 'Battery1', 'Battery2', 'Battery3', 'Battery4'],
-        [0,0,0,0,0]
-    ]);
-    batteryVoltageOptions = {
-        backgroundColor: "#343434",
-        title: "Battery Voltage",
-        titleTextStyle: {color: "white"},
-        legend: {textStyle: {color: "#FFFFFF"}, position: 'in'},
-        hAxis: {title: intervalTitle,titleTextStyle: {color: "white"}, textStyle: {color: "white"}, baselineColor: "white", gridLines: {color: "#FFFFFF"}},
-        vAxis: {title: 'Voltage',titleTextStyle: {color: "white"}, textStyle: {color: "white"}, minValue: 0, maxValue: (batteryMaxVoltage+10)},
-        width: parseInt(computedSize.getPropertyValue('width')),
-        height: parseInt(computedSize.getPropertyValue('height')),
-        chartArea: {width: '70%', height: '85%', left: 70, right: 25},
-        explorer: {
-            actions: ['dragToZoom', 'rightClickToReset'],
-            axis: 'horizontal',
-            keepInBounds: true,
-            maxZoomIn: 10
-        },
-        series: chart_colors
-    }
-
-    batteryVoltageChart = new google.visualization.AreaChart(document.getElementById('battery_voltage'));
-    batteryVoltageChart.draw(batteryVoltageData, batteryVoltageOptions);
-}
-function updateBatteryVoltageChart() { //Call this function per get/post request on .then
-    let newInsert = [timeActive];
-    batteries.forEach(function(battery) {
-        newInsert.push(battery.voltage);
-    });
-    batteryVoltageData.addRow(newInsert);
-    batteryVoltageChart.draw(batteryVoltageData, batteryVoltageOptions);
-}
-
-// BATTERY AMP ------------------------------------------------------------------------------------------
-let batteryAmpData;
-let batteryAmpOptions;
-let batteryAmpChart;
-
-function initBatteryAmpChart() {
-    const computedSize = window.getComputedStyle(document.getElementById('example_chart_size'));
-    batteryAmpData = google.visualization.arrayToDataTable([
-        ['3 Second Interval', 'Battery1', 'Battery2', 'Battery3', 'Battery4'],
-        [0,0,0,0,0]
-    ]);
-    batteryAmpOptions = {
-        backgroundColor: "#343434",
-        title: "Battery Amps",
-        titleTextStyle: {color: "white"},
-        legend: {textStyle: {color: "#FFFFFF"}, position: 'in'},
-        hAxis: {title: intervalTitle,titleTextStyle: {color: "white"}, textStyle: {color: "white"}, baselineColor: "white", gridLines: {color: "#FFFFFF"}},
-        vAxis: {title: 'Amps',titleTextStyle: {color: "white"}, textStyle: {color: "white"}, minValue: 0, maxValue: (batteryMaxAmps+10)},
-        width: parseInt(computedSize.getPropertyValue('width')),
-        height: parseInt(computedSize.getPropertyValue('height')),
-        chartArea: {width: '70%', height: '85%', left: 70, right: 25},
-        explorer: {
-            actions: ['dragToZoom', 'rightClickToReset'],
-            axis: 'horizontal',
-            keepInBounds: true,
-            maxZoomIn: 10
-        },
-        series: chart_colors
-    }
-
-    batteryAmpChart = new google.visualization.AreaChart(document.getElementById('battery_amp'));
-    batteryAmpChart.draw(batteryAmpData, batteryAmpOptions);
-}
-function updateBatteryAmpChart() { //Call this function per get/post request on .then
-    let newInsert = [timeActive];
-    batteries.forEach(function(battery) {
-        newInsert.push(battery.amps);
-    });
-    batteryAmpData.addRow(newInsert);
-    batteryAmpChart.draw(batteryAmpData, batteryAmpOptions);
-}
-
-// MOTOR PWM ---------------------------------------------------------------------------------------------
-
-let motorData;
-let motorOptions;
-let motorChart;
-function initMotorChart() {
-    const computedSize = window.getComputedStyle(document.getElementById('example_chart_size'));
-    motorData = google.visualization.arrayToDataTable([
-        ['3 Second Interval', 'Motor1', 'Motor2', 'Motor3', 'Motor4', 'Motor5', 'Motor 6', 'Motor 7', 'Motor 8'],
-        [0,0,0,0,0,0,0,0,0]
-    ]);
-    motorOptions = {
-        backgroundColor: "#343434",
-        title: "Motor PWM",
-        titleTextStyle: {color: "white"},
-        legend: {textStyle: {color: "#FFFFFF"}, position: 'in'},
-        hAxis: {title: intervalTitle,titleTextStyle: {color: "white"}, textStyle: {color: "white"}, baselineColor: "white", gridLines: {color: "#FFFFFF"}},
-        vAxis: {title: 'PWM',titleTextStyle: {color: "white"}, textStyle: {color: "white"}, minValue: 0, maxValue: (motorMaxPWM+10)},
-        width: parseInt(computedSize.getPropertyValue('width')),
-        height: parseInt(computedSize.getPropertyValue('height')),
-        chartArea: {width: '70%', height: '85%', left: 70, right: 25},
-        explorer: {
-            actions: ['dragToZoom', 'rightClickToReset'],
-            axis: 'horizontal',
-            keepInBounds: true,
-            maxZoomIn: 10
-        },
-        series: chart_colors
-    }
-
-    motorChart = new google.visualization.AreaChart(document.getElementById('motor_pwm'));
-    motorChart.draw(motorData, motorOptions);
-}
-function updateMotorChart() { //Call this function per get/post request on .then
-    let newInsert = [timeActive];
-    motors.forEach(function(motor) {
-        newInsert.push(motor.pwm);
-    });
-    motorData.addRow(newInsert);
-    motorChart.draw(motorData, motorOptions);
-}
-
-// SERVER PWM --------------------------------------------------------------------------------------------
-
-let servoData;
-let servoOptions;
-let servoChart;
-function initServoChart() {
-    const computedSize = window.getComputedStyle(document.getElementById('example_chart_size'));
-    servoData = google.visualization.arrayToDataTable([
-        ['3 Second Interval', 'Servo1', 'Servo2'],
-        [0,0,0]
-    ]);
-    servoOptions = {
-        backgroundColor: "#343434",
-        title: "Servo PWM",
-        titleTextStyle: {color: "white"},
-        legend: {textStyle: {color: "#FFFFFF"}, position: 'in'},
-        hAxis: {title: intervalTitle,titleTextStyle: {color: "white"}, textStyle: {color: "white"}, baselineColor: "white", gridLines: {color: "#FFFFFF"}},
-        vAxis: {title: 'PWM',titleTextStyle: {color: "white"}, textStyle: {color: "white"}, minValue: 0, maxValue: (motorMaxPWM+10)},
-        width: parseInt(computedSize.getPropertyValue('width')),
-        height: parseInt(computedSize.getPropertyValue('height')),
-        chartArea: {width: '70%', height: '85%', left: 70, right: 25},
-        explorer: {
-            actions: ['dragToZoom', 'rightClickToReset'],
-            axis: 'horizontal',
-            keepInBounds: true,
-            maxZoomIn: 10
-        },
-        series: chart_colors
-    }
-
-    servoChart = new google.visualization.AreaChart(document.getElementById('servo_pwm'));
-    servoChart.draw(servoData, servoOptions);
-}
-function updateServoChart() { //Call this function per get/post request on .then
-    let newInsert = [timeActive];
-    servos.forEach(function(servo) {
-        newInsert.push(servo.pwm);
-    });
-    servoData.addRow(newInsert);
-    servoChart.draw(servoData, servoOptions);
-}
-
-
-// CHART MASTER FUNCTIONS -------------------------------------------------------------------
-let batteryVoltageChartSelected = false;
-let batteryAmpChartSelected = false;
-let motorChartSelected = false;
-let servoChartSelected = false;
-function data_charts(callback) {
-    console.log("Loaded")
-    initBatteryVoltageChart();
-    initBatteryAmpChart();
-    initMotorChart();
-    initServoChart();
-
-    chartSelections(batteryVoltageChart, batteryVoltageData, batteryVoltageOptions, batteryVoltageChartSelected);
-    chartSelections(batteryAmpChart, batteryAmpData, batteryAmpOptions, batteryAmpChartSelected);
-    chartSelections(motorChart, motorData, motorOptions, motorChartSelected);
-    chartSelections(servoChart, servoData, servoOptions, servoChartSelected);
-}
-
-function chartSelections(chart, data, options, selectionBool) {
-    google.visualization.events.addListener(chart, 'click', function () {
-        let series = JSON.parse(JSON.stringify(chart_colors));
-        setTimeout(() => {
-            let selectedItem = chart.getSelection();
-            if(!selectedItem) {
-                options.series = series;
-                chart.draw(data, options);
-                return;
-            }
-            if(!selectionBool) {
-                if (selectedItem.length > 0) {
-                    let columnIndex = selectedItem[0].column - 1;
-                    if (columnIndex != null) {
-                        for (let i = 0; i < data.getNumberOfColumns(); i++) {
-                            if (i !== columnIndex) {
-                                series[i] = {color: 'transparent'};
-                            }
-                        }
-                        options.series = series;
-                        chart.draw(data, options);
-                    }
-                    selectionBool = true;
-                }
-            } else {
-                options.series = series;
-                chart.draw(data, options);
-                selectionBool = false;
-            }
-        }, 40)
-    });
-}
-// ------------------------------------------------- LOG PAGE -------------------------------------------------
+// ------------------------ END SERVO DATA | START LOG PAGE  ----------------------------
 
 function highlight_message(message_container) {
     if(message_container.classList.contains('marked')) { message_container.classList.remove('marked');
