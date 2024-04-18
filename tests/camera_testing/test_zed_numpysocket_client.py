eea66fda035afd50234fcb1f855b59f7026f9c03
Lees <msle237.lees@gmail.com>
2024-04-17 22:43:47 -0400

    camera testing

--- tests/camera_testing/test_zed_numpysocket_client.py
+++ tests/camera_testing/test_zed_numpysocket_client.py
@@ -5,12 +5,36 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+# Resizes a image and maintains aspect ratio
+def maintain_aspect_ratio_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # Grab the image size and initialize dimensions
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # Return original image if no need to resize
+    if width is None and height is None:
+        return image
+
+    # We are resizing height if width is none
+    if width is None:
+        # Calculate the ratio of the height and construct the dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    # We are resizing width if height is none
+    else:
+        # Calculate the ratio of the width and construct the dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # Return the resized image
+    return cv2.resize(image, dim, interpolation=inter)
+
 with NumpySocket() as s:
     s.connect(("10.0.0.163", 9999))
     while cap.isOpened():
         ret, frame = cap.read()
-        ref_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_resize = ref_frame[::2, ::2]
+        # ref_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_resize = maintain_aspect_ratio_resize(frame, width=480)
         if ret is True:
             try:
                 s.sendall(frame_resize)
--- tests/camera_testing/test_zed_numpysocket_server.py
+++ tests/camera_testing/test_zed_numpysocket_server.py
@@ -8,8 +8,32 @@ import cv2
 logger = logging.getLogger("OpenCV server")
 logger.setLevel(logging.INFO)
 
+# Resizes a image and maintains aspect ratio
+def maintain_aspect_ratio_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # Grab the image size and initialize dimensions
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # Return original image if no need to resize
+    if width is None and height is None:
+        return image
+
+    # We are resizing height if width is none
+    if width is None:
+        # Calculate the ratio of the height and construct the dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    # We are resizing width if height is none
+    else:
+        # Calculate the ratio of the width and construct the dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # Return the resized image
+    return cv2.resize(image, dim, interpolation=inter)
+
 with NumpySocket() as s:
-    s.bind(("", 9999))
+    s.bind(("0.0.0.0", 9999))
 
     while True:
         try:
@@ -21,6 +45,8 @@ with NumpySocket() as s:
                 frame = conn.recv()
                 if len(frame) == 0:
                     break
+
+                frame = maintain_aspect_ratio_resize(frame, width=1920)
 
                 cv2.imshow("Frame", frame)
 
