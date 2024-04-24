c7f043796701a263571b84c99747b1888bf7ad19
Lees <msle237.lees@gmail.com>
2024-04-24 18:17:40 -0400

    hardware interface

--- modules/HardwareInterface.py
+++ modules/HardwareInterface.py
@@ -75,5 +75,3 @@ if __name__ == '__main__':
     # write_ESCs([127, 127, 127, 127, 127, 127, 127, 127])
     for i in range(0, 256):
         write_BatteryMonitor([i, 0, 1])
-    for i in range(256, 0):
-        write_BatteryMonitor([i, 0, 1])
