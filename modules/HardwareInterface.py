1c1bb5115fb3ff0e97cd4033a4f44a73cd3127b1
Lees <msle237.lees@gmail.com>
2024-04-26 14:29:13 -0400

    fixing the hardware interface

--- modules/HardwareInterface.py
+++ modules/HardwareInterface.py
@@ -11,8 +11,7 @@ class HardwareInterface:
         with open('./configs/hardware_interface.json') as f:
             self.config = json.load(f)
 
-        self.hardwareDBAddress = self.config['hardwareDBAddress']
-        self.hardwareDBPort = self.config['hardwareDBPort']
+        self.baseUrl = self.config["base_url"]
 
     def write_ESCs(self, data = [127, 127, 127, 127, 127, 127, 127, 127]):
         device_address = 0x21
