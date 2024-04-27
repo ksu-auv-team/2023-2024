225c1974e6ed847abdf12bcb5e092070f56a916c
Lees <msle237.lees@gmail.com>
2024-04-26 22:10:55 -0400

    hardware interface

--- modules/MovementPackage.py
+++ modules/MovementPackage.py
@@ -365,12 +365,14 @@ class MovementPackage:
     def run(self):
         while True:
             self.controller_data = self.get_data()
+            print(self.controller_data)
             # self.sensors_data = self.get_sensors_data()
             #  and self.sensors_data
             if self.controller_data:
+                pass
                 # self.neural_network_data = self.neural_network_data
-                self.output_control_data_part_1, self.output_control_data_part_2 = self.convert_to_motor_values(np.array([self.controller_data["X"], self.controller_data["Y"], self.controller_data["Z"], self.controller_data["pitch"], self.controller_data["roll"], self.controller_data["yaw"]]))
-                self.save_data(self.output_control_data_part_1, self.output_control_data_part_2, 127, 0, 0)
+                # self.output_control_data_part_1, self.output_control_data_part_2 = self.convert_to_motor_values(np.array([self.controller_data["X"], self.controller_data["Y"], self.controller_data["Z"], self.controller_data["pitch"], self.controller_data["roll"], self.controller_data["yaw"]]))
+                # self.save_data(self.output_control_data_part_1, self.output_control_data_part_2, 127, 0, 0)
             else:
                 self.movement_logger.error("Failed to get controller or sensors data")
             time.sleep(0.01)
