deb5e3f9232827401784233c82e886e9ce27a37a
surgosan <sergy200118@gmail.com>
2024-04-28 01:08:20 -0400

    Testing Local API XXIX  |  Global HTTP error handler XI

--- control_console/src/App.vue
+++ control_console/src/App.vue
@@ -69,6 +69,7 @@
         console.log("Unknown Error: ", error);
       }
     }
+    await store.dispatch('relayErrors');
   }
 
   const powerButton = async () => { //Make async when adding http requests
