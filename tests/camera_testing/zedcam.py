196c4321a9b4aaaf657f2408cce8736e46549994
msle237-lees <msle237.lees@gmail.com>
2024-06-08 11:45:46 -0400

    camera testing

--- tests/camera_testing/zedcam.py
+++ tests/camera_testing/zedcam.py
@@ -1,8 +1,13 @@
 import cv2
 
 
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 if cam.isOpened():
     continue
 else:
     print('failed')
+
+while True:
+    ret, frame = cam.read()
+
+    print(len(frame))
