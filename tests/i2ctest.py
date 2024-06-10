ba108e6f02f368b02a7132ea6b84a01d0d3f4f68
Lees <msle237.lees@gmail.com>
2024-06-10 17:26:14 -0400

    test for i2c

---
+++ tests/i2ctest.py
@@ -0,0 +1,12 @@
+from smbus2 import SMBus
+import time
+while True:
+    try:
+        # Open i2c bus 1 and read one byte from address 80, offset 0
+        bus = SMBus(7)
+        b = bus.read_byte_data(9, 0)
+        print(b)
+        bus.close()
+    except Exception as e:
+        print(e)
+        time.sleep(1)
