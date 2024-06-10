b526603fd8641c40b72adcae9c503952e913aa68
Lees <msle237.lees@gmail.com>
2024-06-10 17:56:16 -0400

    i2c test

--- tests/i2ctest.py
+++ tests/i2ctest.py
@@ -6,7 +6,7 @@ while True:
         # Open i2c bus 1 and read one byte from address 80, offset 0
         bus = SMBus(7)
         # b = bus.read_byte_data(8, 0)
-        b = bus.write_i2c_block_data(0x08,  0, data)
+        b = bus.write_i2c_block_data(8,  0, data)
         print(b)
         bus.close()
     except Exception as e:
