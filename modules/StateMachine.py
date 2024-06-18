e4de4e97c1863544603854f7ebb66c3729d7d3db
Lees <msle237.lees@gmail.com>
2024-06-18 10:16:37 -0400

    state machine changes

--- modules/StateMachine.py
+++ modules/StateMachine.py
@@ -5,98 +5,105 @@ import json
 import logging
 import requests
 
-class StateMachine(StateMachine):
+class AUVStateMachine(StateMachine):
     # Define the states
-    Idle = State('Idle')
-    Scan = State('Scan')
-    Movement = State('Movement')
-    Locate = State('Locate')
-    Grab = State('Grab')
-    Release = State('Release')
-    Torpedo_Launch_1 = State('Torpedo_Launch_1')
-    Torpedo_Launch_2 = State('Torpedo_Launch_2')
-    Object_Detection_Algorithm_1 = State('Object_Detection_Algorithm_1')
-    Object_Detection_Algorithm_2 = State('Object_Detection_Algorithm_2')
-    Object_Detection_Algorithm_3 = State('Object_Detection_Algorithm_3')
-    Object_Detection_Algorithm_4 = State('Object_Detection_Algorithm_4')
-    Object_Detection_Algorithm_5 = State('Object_Detection_Algorithm_5')
-    Object_Detection_Algorithm_6 = State('Object_Detection_Algorithm_6')
-    Mission_1 = State('Mission_1', initial=True)
-    Mission_2 = State('Mission_2')
-    Mission_3 = State('Mission_3')
-    Mission_4 = State('Mission_4')
-    Mission_5 = State('Mission_5')
+    # Start and End States
+    Start = State('Start', initial=True)
     End = State('End')
     
-    # Define the transitions
-    # Mission 1
-    Mission_1_to_Idle = Mission_1.to(Idle)
-    Idle_to_Scan = Idle.to(Scan)
-    Scan_to_Object_Detection_Algorithm_1 = Scan.to(Object_Detection_Algorithm_1)
-    Object_Detection_Algorithm_1_to_Movement = Object_Detection_Algorithm_1.to(Movement)
-    Movement_to_Locate = Movement.to(Locate)
-    Locate_to_Scan = Locate.to(Scan)
-    Scan_to_Object_Detection_Algorithm_2 = Scan.to(Object_Detection_Algorithm_2)
-    Scan_to_Movement = Scan.to(Movement)
-    Movement_to_Movement = Movement.to(Movement)
-    Movement_to_Idle = Movement.to(Idle)
-    Idle_to_Mission_2 = Idle.to(Mission_2)
+    # Idle State
+    Idle = State('Idle')
     
-    # Mission 2
-    Mission_2_to_Idle = Mission_2.to(Idle)
-    Idle_to_Movement = Idle.to(Movement)
-    Movement_to_Scan = Movement.to(Scan)
-    Scan_to_Object_Detection_Algorithm_3 = Scan.to(Object_Detection_Algorithm_3)
-    Object_Detection_Algorithm_3_to_Movement = Object_Detection_Algorithm_3.to(Movement)
-    Movement_to_Object_Detection_Algorithm_3 = Movement.to(Object_Detection_Algorithm_3)
-    Movement_to_Idle = Movement.to(Idle)
-    Idle_to_Mission_3 = Idle.to(Mission_3)
+    # Movement States
+    Quarter_Speed_Forward = State('Quarter_Speed_Forward')
+    Half_Speed_Forward = State('Half_Speed_Forward')
+    Full_Speed_Forward = State('Full_Speed_Forward')
     
-    # Mission 3
-    Mission_3_to_Idle = Mission_3.to(Idle)
-    Idle_to_Scan = Idle.to(Scan)
-    Scan_to_Object_Detection_Algorithm_1 = Scan.to(Object_Detection_Algorithm_1)
-    Object_Detection_Algorithm_1_to_Movement = Object_Detection_Algorithm_1.to(Movement)
-    Movement_to_Grab = Movement.to(Grab)
-    Grab_to_Movement = Grab.to(Movement)
-    Movement_to_Scan = Movement.to(Scan)
-    Scan_to_Object_Detection_Algorithm_4 = Scan.to(Object_Detection_Algorithm_4)
-    Object_Detection_Algorithm_4_to_Movement = Object_Detection_Algorithm_4.to(Movement)
-    Movement_to_Release = Movement.to(Release)
-    Release_to_Movement = Release.to(Movement)
-    Movement_to_Idle = Movement.to(Idle)
-    Idle_to_Mission_4 = Idle.to(Mission_4)
+    Quarter_Speed_Backward = State('Quarter_Speed_Backward')
+    Half_Speed_Backward = State('Half_Speed_Backward')
+    Full_Speed_Backward = State('Full_Speed_Backward')
     
-    # Mission 4
-    Mission_4_to_Idle = Mission_4.to(Idle)
-    Idle_to_Scan = Idle.to(Scan)
-    Scan_to_Object_Detection_Algorithm_1 = Scan.to(Object_Detection_Algorithm_1)
-    Object_Detection_Algorithm_1_to_Movement = Object_Detection_Algorithm_1.to(Movement)
-    Movement_to_Object_Detection_Algorithm_5 = Movement.to(Object_Detection_Algorithm_5)
-    Object_Detection_Algorithm_5_to_Torpedo_Launch_1 = Object_Detection_Algorithm_5.to(Torpedo_Launch_1)
-    Torpedo_Launch_1_to_Movement = Torpedo_Launch_1.to(Movement)
-    Movement_to_Torpedo_Launch_2 = Movement.to(Torpedo_Launch_2)
-    Torpedo_Launch_2_to_Movement = Torpedo_Launch_2.to(Movement)
-    Movement_to_Idle = Movement.to(Idle)
-    Idle_to_Mission_5 = Idle.to(Mission_5)
+    Quarter_Speed_Turn_Left = State('Quarter_Speed_Turn_Left')
+    Half_Speed_Turn_Left = State('Half_Speed_Turn_Left')
+    Full_Speed_Turn_Left = State('Full_Speed_Turn_Left')
     
-    # Mission 5
-    Mission_5_to_Idle = Mission_5.to(Idle)
+    Quarter_Speed_Turn_Right = State('Quarter_Speed_Turn_Right')
+    Half_Speed_Turn_Right = State('Half_Speed_Turn_Right')
+    Full_Speed_Turn_Right = State('Full_Speed_Turn_Right')
+    
+    Quarter_Speed_Lateral_Left = State('Quarter_Speed_Lateral_Left')
+    Half_Speed_Lateral_Left = State('Half_Speed_Lateral_Left')
+    Full_Speed_Lateral_Left = State('Full_Speed_Lateral_Left')
+    
+    Quarter_Speed_Lateral_Right = State('Quarter_Speed_Lateral_Right')
+    Half_Speed_Lateral_Right = State('Half_Speed_Lateral_Right')
+    Full_Speed_Lateral_Right = State('Full_Speed_Lateral_Right')
+    
+    Quarter_Speed_Vertical_Up = State('Quarter_Speed_Vertical_Up')
+    Half_Speed_Vertical_Up = State('Half_Speed_Vertical_Up')
+    Full_Speed_Vertical_Up = State('Full_Speed_Vertical_Up')
+    
+    Quarter_Speed_Vertical_Down = State('Quarter_Speed_Vertical_Down')
+    Half_Speed_Vertical_Down = State('Half_Speed_Vertical_Down')
+    Full_Speed_Vertical_Down = State('Full_Speed_Vertical_Down')
+    
+    Quarter_Speed_Pitch_Up = State('Quarter_Speed_Pitch_Up')
+    Half_Speed_Pitch_Up = State('Half_Speed_Pitch_Up')
+    Full_Speed_Pitch_Up = State('Full_Speed_Pitch_Up')
+    
+    Quarter_Speed_Pitch_Down = State('Quarter_Speed_Pitch_Down')
+    Half_Speed_Pitch_Down = State('Half_Speed_Pitch_Down')
+    Full_Speed_Pitch_Down = State('Full_Speed_Pitch_Down')
+    
+    # Scan States
+    Scan = State('Scan')
+    
+    # Gripper States
+    Grab = State('Grab')
+    Release = State('Release')
+    
+    # Torpedo States
+    Torpedo_Launch_1 = State('Torpedo_Launch_1')
+    Torpedo_Launch_2 = State('Torpedo_Launch_2')
+    
+    # Define the transitions between states
+    Start_to_Idle = Start.to(Idle)
+    
+    # Mission 1 Transitions
+    Idle_to_Quarter_Speed_Turn_Left = Idle.to(Quarter_Speed_Turn_Left)
+    Quarter_Speed_Turn_Left_to_Quarter_Speed_Right = Quarter_Speed_Turn_Left.to(Quarter_Speed_Turn_Right)
+    Quarter_Speed_Turn_Right_to_Quarter_Speed_Turn_Left = Quarter_Speed_Turn_Right.to(Quarter_Speed_Turn_Left)
+    Quarter_Speed_Turn_Left_to_Idle = Quarter_Speed_Turn_Left.to(Idle)
+    Idle_to_Half_Speed_Forward = Idle.to(Half_Speed_Forward)
+    Half_Speed_Forward_to_Idle = Half_Speed_Forward.to(Idle)
     Idle_to_Scan = Idle.to(Scan)
-    Scan_to_Object_Detection_Algorithm_1 = Scan.to(Object_Detection_Algorithm_1)
-    Object_Detection_Algorithm_1_to_Movement = Object_Detection_Algorithm_1.to(Movement)
-    Movement_to_Object_Detection_Algorithm_6 = Movement.to(Object_Detection_Algorithm_6)
-    Object_Detection_Algorithm_6_to_Movement = Object_Detection_Algorithm_6.to(Movement)
-    Movement_to_Grab = Movement.to(Grab)
-    Grab_to_Movement = Grab.to(Movement)
-    Movement_to_Release = Movement.to(Release)
-    Release_to_Movement = Release.to(Movement)
-    Movement_to_Idle = Movement.to(Idle)
-    Idle_to_End = Idle.to(End)
+    Scan_to_Quarter_Speed_Vertical_Up = Scan.to(Quarter_Speed_Vertical_Up)
+    Quarter_Speed_Vertical_Up_to_Idle = Quarter_Speed_Vertical_Up.to(Idle)
+    Idle_to_Quarter_Speed_Lateral_Right = Idle.to(Quarter_Speed_Lateral_Right)
+    Quarter_Speed_Lateral_Right_to_Idle = Quarter_Speed_Lateral_Right.to(Idle)
+    Idle_to_Quarter_Speed_Forward = Idle.to(Quarter_Speed_Forward)
+    Quarter_Speed_Forward_to_Idle = Quarter_Speed_Forward.to(Idle)
+    Idle_to_Scan = Idle.to(Scan)
+    Scan_to_Idle = Scan.to(Idle)
+    Idle_to_Quarter_Speed_Backward = Idle.to(Quarter_Speed_Backward)
+    Quarter_Speed_Backward_to_Idle = Quarter_Speed_Backward.to(Idle)
+    Idle_to_Quarter_Speed_Lateral_Left = Idle.to(Quarter_Speed_Lateral_Left)
+    Quarter_Speed_Lateral_Left_to_Idle = Quarter_Speed_Lateral_Left.to(Idle)
+    Idle_to_Quarter_Speed_Forward = Idle.to(Quarter_Speed_Forward)
+    Quarter_Speed_Forward_to_Idle = Quarter_Speed_Forward.to(Idle)
+    Idle_to_Half_Speed_Forward = Idle.to(Half_Speed_Forward)
+    Half_Speed_Forward_to_Idle = Half_Speed_Forward.to(Idle)
+    Idle_to_Quarter_Speed_Turn_Right = Idle.to(Quarter_Speed_Turn_Right)
+    Quarter_Speed_Turn_Right_to_Idle = Quarter_Speed_Turn_Right.to(Idle)
+    Idle_to_Quarter_Speed_Forward = Idle.to(Quarter_Speed_Forward)
+    Quarter_Speed_Forward_to_Idle = Quarter_Speed_Forward.to(Idle)
+    
+    # Mission 2 Transitions
+    
     
 
 if __name__ == '__main__':
-    m = StateMachine()
+    m = AUVStateMachine()
     graph = DotGraphMachine(m)
     dot = graph()
     dot.write_png('static/docs/state_machine.png')
