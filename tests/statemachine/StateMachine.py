e6ebc730690244e28e1a2520e724db4e22bb2b33
Lees <msle237.lees@gmail.com>
2024-06-23 21:35:41 -0400

    moved statemachine code to the test folder

---
+++ tests/statemachine/StateMachine.py
@@ -0,0 +1,17 @@
+from statemachine import StateMachine, State
+from statemachine.contrib.diagram import DotGraphMachine
+
+import json
+import logging
+import requests
+
+# Import the necessary modules from the statemachine_modules folder
+
+class AUVStateMachine(StateMachine):
+    pass
+
+if __name__ == '__main__':
+    m = AUVStateMachine()
+    graph = DotGraphMachine(m)
+    dot = graph()
+    dot.write_png('static/docs/pngs/AUVStateMachine.png')
--- tests/statemachine/stateMachineModules/basicMissions.py
+++ tests/statemachine/stateMachineModules/basicMissions.py
@@ -5,6 +5,30 @@ import json
 import logging
 import requests
 
+def processMission1():
+    pass
+
+def processMission2():
+    pass
+
+def processMission3():
+    pass
+
+def processMission4():
+    pass
+
+def processMission5():
+    pass
+
+def getSensorData():
+    pass
+
+def getObjectData():
+    pass
+
+def sendInputData():
+    pass
+
 
 class basicMissions(StateMachine):
     # Setup the logging to a file
@@ -47,50 +71,40 @@ class basicMissions(StateMachine):
     idle_to_mission_5 = idle.to(mission_5)
     mission_5_to_stop = mission_5.to(stop)
     
-    def processMission1(self):
-        pass
-    
-    def processMission2(self):
-        pass
-    
-    def processMission3(self):
-        pass
-    
-    def processMission4(self):
-        pass
-    
-    def processMission5(self):
-        pass
-    
     def on_start_to_idle(self):
         logging.info('Starting the AUV')
         
     def on_idle_to_mission_1(self):
         logging.info('Starting Mission 1')
+        self.processMission1()
         
     def on_mission_1_to_idle(self):
         logging.info('Mission 1 complete')
         
     def on_idle_to_mission_2(self):
         logging.info('Starting Mission 2')
+        self.processMission2()
     
     def on_mission_2_to_idle(self):
         logging.info('Mission 2 complete')
         
     def on_idle_to_mission_3(self):
         logging.info('Starting Mission 3')
+        self.processMission3()
         
     def on_mission_3_to_idle(self):
         logging.info('Mission 3 complete')
         
     def on_idle_to_mission_4(self):
         logging.info('Starting Mission 4')
+        self.processMission4()
         
     def on_mission_4_to_idle(self):
         logging.info('Mission 4 complete')
         
     def on_idle_to_mission_5(self):
         logging.info('Starting Mission 5')
+        self.processMission5()
         
     def on_mission_5_to_stop(self):
         logging.info('Mission 5 complete. Stopping the AUV')
@@ -100,6 +114,18 @@ class basicMissions(StateMachine):
         
 if __name__ == '__main__':
     m = basicMissions()
-    graph = DotGraphMachine(m)
-    dot = graph()
-    dot.write_png('static/docs/basicMissions.png')
+    # graph = DotGraphMachine(m)
+    # dot = graph()
+    # dot.write_png('static/docs/basicMissions.png')
+    
+    while True:
+        sensor_data = getSensorData()
+        object_data = getObjectData()
+        
+        if (sensor_data['voltage1'] or sensor_data['voltage2'] or sensor_data['voltage3']) < 10:
+            m.stop()
+            break
+        elif sensor_data['error']:
+            m.stop()
+            break
+        
