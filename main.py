e5f5b565820a1611920ffc8f7a8d3ee3567c4f27
Lees <msle237.lees@gmail.com>
2024-06-27 17:17:46 -0400

    simulation testing

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 # Importing the necessary libraries
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import subprocess
 import argparse
 import time
@@ -45,205 +46,68 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__fil
 # Ensure the upload directory exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-class Sensors(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    voltage1 = db.Column(db.Float, nullable=True)
-    voltage2 = db.Column(db.Float, nullable=True)
-    voltage3 = db.Column(db.Float, nullable=True)
-    current1 = db.Column(db.Float, nullable=True)
-    current2 = db.Column(db.Float, nullable=True)
-    current3 = db.Column(db.Float, nullable=True)
-    error = db.Column(db.Integer, nullable=True)
-    depth = db.Column(db.Float, nullable=True)
-    X = db.Column(db.Float, nullable=True)
-    Y = db.Column(db.Float, nullable=True)
-    Z = db.Column(db.Float, nullable=True)
-    pitch = db.Column(db.Float, nullable=True)
-    roll = db.Column(db.Float, nullable=True)
-    yaw = db.Column(db.Float, nullable=True)
-    temperature = db.Column(db.Float, nullable=True)
-    orin_temp = db.Column(db.Float, nullable=True)
-    humidity = db.Column(db.Float, nullable=True)
-    heading = db.Column(db.String(15), nullable=True)
+# Create the database models for image uploads and movement outputs
+class ImageUpload(db.Model):
+    time = db.Column(db.DateTime, primary_key=True)
+    img1 = db.Column(db.Bytea, nullable=False)
+    img2 = db.Column(db.Bytea, nullable=False)
+    img3 = db.Column(db.Bytea, nullable=False)
+
+    def __init__(self, time, img1, img2, img3):
+        self.time = time
+        self.img1 = img1
+        self.img2 = img2
+        self.img3 = img3
 
     def __repr__(self):
-        return f'<{self.id}, {self.voltage1}, {self.voltage2}, \
-                  {self.volage3}, {self.current1}, {self.current2}, \
-                  {self.current3}, {self.depth}, {self.X}, \
-                  {self.Y}, {self.Z}, {self.pitch}, \
-                  {self.roll}, {self.yaw}, {self.temperature}, \
-                  {self.orin_temp}, {self.humidity}, {self.heading}>'
+        return f"ImageUpload('{self.time}')"
 
 class Output(db.Model):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    M1 = db.Column(db.Integer, nullable=False)
-    M2 = db.Column(db.Integer, nullable=False)
-    M3 = db.Column(db.Integer, nullable=False)
-    M4 = db.Column(db.Integer, nullable=False)
-    M5 = db.Column(db.Integer, nullable=False)
-    M6 = db.Column(db.Integer, nullable=False)
-    M7 = db.Column(db.Integer, nullable=False)
-    M8 = db.Column(db.Integer, nullable=False)
-    Claw = db.Column(db.Integer, nullable=False)
-    Torp1 = db.Column(db.Boolean, nullable=False)
-    Torp2 = db.Column(db.Boolean, nullable=False)
+    time = db.Column(db.DateTime, primary_key=True)
+    direction = db.Column(db.String(2), nullable=False)
+    speed = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, time, output):
+        self.time = time
+        self.direction = output[0]
+        self.speed = output[1]
 
     def __repr__(self):
-        return f'<{self.id}, {self.M1}, {self.M2}, \
-                  {self.M3}, {self.M4}, {self.M5}, \
-                  {self.M6}, {self.M7}, {self.M8}, \
-                  {self.Claw}, {self.Torp1}, {self.Torp2}>'
+        return f"Output('{self.time}')"
 
-class Input(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    X = db.Column(db.Float, nullable=False)
-    Y = db.Column(db.Float, nullable=False)
-    Z = db.Column(db.Float, nullable=False)
-    pitch = db.Column(db.Float, nullable=False)
-    roll = db.Column(db.Float, nullable=False)
-    yaw = db.Column(db.Float, nullable=False)
-    claw = db.Column(db.Integer, nullable=False)
-    torp1 = db.Column(db.Boolean, nullable=False)
-    torp2 = db.Column(db.Boolean, nullable=False)
-
-    def __repr__(self):
-        return f'<{self.id}, {self.X}, {self.Y}, \
-                  {self.Z}, {self.pitch}, {self.roll}, \
-                  {self.yaw}, {self.claw}, {self.torp1}, \
-                  {self.torp2}>'
-                  
-class Objects(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    object = db.Column(db.String(50), nullable=False)
-    distance = db.Column(db.Float, nullable=False)
-    angle = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f'<{self.id}, {self.object}, {self.distance}, {self.angle}>'
-
-# Creating the flask routes to handle the data
-@app.route('/sensors', methods=['POST'])
-def add_sensor_data():
-    data = request.get_json()
-
-    new_sensor_data = Sensors(voltage1=data['voltage1'], voltage2=data['voltage2'], voltage3=data['voltage3'],
-                              current1=data['current1'], current2=data['current2'], current3=data['current3'],
-                              depth=data['depth'], X=data['X'], Y=data['Y'], Z=data['Z'],
-                              pitch=data['pitch'], roll=data['roll'], yaw=data['yaw'],
-                              temperature=data['temperature'], orin_temp=data['orin_temp'], humidity=data['humidity'],
-                              heading=data['heading'])
-
-    db.session.add(new_sensor_data)
-    db.session.commit()
-
-    return 'Data added', 201
-
-@app.route('/sensors', methods=['GET'])
-def get_sensor_data():
-    sensor_data = Sensors.query.order_by(Sensors.id.desc()).first()
-    if sensor_data:
-        data_dict = {
-            'voltage1': sensor_data.voltage1, 'voltage2': sensor_data.voltage2, 'voltage3': sensor_data.voltage3,
-            'current1': sensor_data.current1, 'current2': sensor_data.current2, 'current3': sensor_data.current3,
-            'depth': sensor_data.depth, 'X': sensor_data.X, 'Y': sensor_data.Y, 'Z': sensor_data.Z,
-            'pitch': sensor_data.pitch, 'roll': sensor_data.roll, 'yaw': sensor_data.yaw,
-            'temperature': sensor_data.temperature, 'orin_temp': sensor_data.orin_temp, 'humidity': sensor_data.humidity,
-            'heading': sensor_data.heading
-        }
-        return jsonify(data_dict)
-    else:
-        return jsonify({'message': 'No data found'}), 404
-
-@app.route('/output', methods=['POST'])
-def add_output_data():
-    data = request.get_json()
-
-    new_output_data = Output(M1=data['M1'], M2=data['M2'], M3=data['M3'], M4=data['M4'],
-                             M5=data['M5'], M6=data['M6'], M7=data['M7'], M8=data['M8'],
-                             Claw=data['Claw'], Torp1=data['Torp1'], Torp2=data['Torp2'])
-
-    db.session.add(new_output_data)
-    db.session.commit()
-
-    return 'Data added', 201
-
-@app.route('/output', methods=['GET'])
-def get_output_data():
-    output_data = Output.query.order_by(Output.id.desc()).first()
-    if output_data:
-        data_dict = {
-            'M1': output_data.M1, 'M2': output_data.M2, 'M3': output_data.M3, 'M4': output_data.M4,
-            'M5': output_data.M5, 'M6': output_data.M6, 'M7': output_data.M7, 'M8': output_data.M8,
-            'Claw': output_data.Claw, 'Torp1': output_data.Torp1, 'Torp2': output_data.Torp2
-        }
-        return jsonify(data_dict)
-    else:
-        return jsonify({'message': 'No data found'}), 404
-
-@app.route('/input', methods=['POST'])
-def add_input_data():
-    data = request.get_json()
-
-    new_input_data = Input(X=data['X'], Y=data['Y'], Z=data['Z'],
-                           pitch=data['Pitch'], roll=data['Roll'], yaw=data['Yaw'],
-                           claw=data['Claw'], torp1=data['Torpedo_1'], torp2=data['Torpedo_2'])
-
-    db.session.add(new_input_data)
-    db.session.commit()
-    # print(new_input_data)
-
-    return 'Data added', 201
-
-@app.route('/input', methods=['GET'])
-def get_input_data():
-    input_data = Input.query.order_by(Input.id.desc()).first()
-    if input_data:
-        data_dict = {
-            'X': input_data.X, 'Y': input_data.Y, 'Z': input_data.Z,
-            'pitch': input_data.pitch, 'roll': input_data.roll, 'yaw': input_data.yaw,
-            'claw': input_data.claw, 'torp1': input_data.torp1, 'torp2': input_data.torp2
-        }
-        # print(data_dict)
-        return jsonify(data_dict)
-    else:
-        return jsonify({'message': 'No data found'}), 404
-
-@app.route('/objects', methods=['POST'])
-def add_object_data():
-    data = request.get_json()
-
-    new_object_data = Objects(object=data['object'], distance=data['distance'], angle=data['angle'])
-
-    db.session.add(new_object_data)
-    db.session.commit()
-
-    return 'Data added', 201
-
-@app.route('/objects', methods=['GET'])
-def get_object_data():
-    object_data = Objects.query.order_by(Objects.id.desc()).first()
-    if object_data:
-        data_dict = {
-            'object': object_data.object, 
-            'distance': object_data.distance, 
-            'angle': object_data.angle
-        }
-        return jsonify(data_dict)
-    else:
-        return jsonify({'message': 'No data found'}), 404
-
+# def the route for the image upload
 @app.route('/upload', methods=['POST'])
 def upload():
-    for filename in request.files:
-        file = request.files[filename]
-        if file:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-    return 'Images uploaded', 200
+    logging.info("Received image upload request")
+    # Get the images from the request
+    img1 = request.files['img1']
+    img2 = request.files['img2']
+    img3 = request.files['img3']
 
-@app.route('/images/<filename>')
-def images(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    # Save the images to the upload folder
+    img1.save(os.path.join(app.config['UPLOAD_FOLDER'], 'img1.jpg'))
+    img2.save(os.path.join(app.config['UPLOAD_FOLDER'], 'img2.jpg'))
+    img3.save(os.path.join(app.config['UPLOAD_FOLDER'], 'img3.jpg'))
+
+    # Save the images to the database
+    new_upload = ImageUpload(time=datetime.now(), img1=img1.read(), img2=img2.read(), img3=img3.read())
+
+    db.session.add(new_upload)
+    db.session.commit()
+
+    logging.info("Images saved to database")
+
+    return jsonify({'message': 'Images uploaded successfully'})
+
+# def the route for the movement input
+@app.route('/output', methods=['POST'])
+def output():
+    pass
+
+# def the route for the movement output
+@app.route('/output', methods=['GET'])
+def get_output():
+    pass
 
 @app.route('/')
 def index():
@@ -253,7 +117,7 @@ def create_tables():
     db.create_all()
 
 # Main function
-def main(args: list = sys.argv):
+def main(args = sys.argv):
     if args.run:
         if args.P:
             hardware_interface = subprocess.Popen(["python3", "modules/HardwareInterface.py", '--P'])
@@ -275,7 +139,7 @@ def main(args: list = sys.argv):
             movement_package = subprocess.Popen(["python3", "modules/MovementPackage.py", '--P'])
         movement_package = subprocess.Popen(["python3", "modules/MovementPackage.py", '--L'])
     if args.NN and not args.run:
-        if args.P: 
+        if args.P:
             neural_network = subprocess.Popen(["python3", "modules/NeuralNetwork.py", '--P'])
         neural_network = subprocess.Popen(["python3", "modules/NeuralNetwork.py", '--L'])
     if args.CP and not args.run:
@@ -291,21 +155,6 @@ def main(args: list = sys.argv):
     with app.app_context():
         create_tables()
     app.run(debug=True, host="0.0.0.0", port=5000)
-
-    if args.run:
-        hardware_interface.wait()
-        movement_package.wait()
-        neural_network.wait()
-        state_machine.wait()
-        camera_package.wait()
-    if args.HI and not args.run:
-        hardware_interface.wait()
-    if args.MP and not args.run:
-        movement_package.wait()
-    if args.NN and not args.run:
-        neural_network.wait()
-    if args.CP and not args.run:
-        camera_package.wait()
 
 # Running the main function
 if __name__ == "__main__":
