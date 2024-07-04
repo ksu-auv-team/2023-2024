d95cf978574fc8691aa89d6e55bdbe2d5abe687b
GSS <gurbirsingh2025@gmail.com>
2024-07-04 13:36:22 -0400

    flask showing 2 cameras and detection code - added currently inactive detection code - zed cam modified so it will show 2 cams on flask

--- tests/camera_testing/services/WebCamService.py
+++ tests/camera_testing/services/WebCamService.py
@@ -3,6 +3,13 @@ import cv2
 
 
 class WebCam:
+    
+    #giving attributes so each camera has a number and ip making it easier to call across different files
+    def __init__(self, ip=None, camera_number=None):
+        self.ip = ip
+        self.camera_number = camera_number
+        self.capture = None
+
     def get_frame(self, capture):
         while True:
             hasFrame, frame = capture.read()
@@ -11,4 +18,5 @@ class WebCam:
                 raise Exception("Camera frame not obtained")
 
             _, jpeg = cv2.imencode('.jpg', frame)
+            
             return jpeg.tobytes()
--- tests/camera_testing/zedcam.py
+++ tests/camera_testing/zedcam.py
@@ -14,14 +14,23 @@ app.register_blueprint(routes.get_blueprint())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
+@app.route('/video_0')
+def video_0():
+    try:
+        webcam0 = WebCam()
+        ip_camera_url_0 = "http://10.0.0.233:5000/stream"  # Replace with your IP camera URL
+        return Response(routes.gen_ip(webcam0, ip_camera_url_0), mimetype='multipart/x-mixed-replace; boundary=frame')
+    except Exception as err:
+        return Response(f'Error {err}')
+    
 @app.route('/video_1')
 def video_1():
     try:
-        webcam = WebCam()
-        ip_camera_url = "http://10.0.0.233:5000/stream"  # Replace with your IP camera URL
-        return Response(routes.gen_ip(webcam, ip_camera_url), mimetype='multipart/x-mixed-replace; boundary=frame')
+        webcam1 = WebCam()
+        ip_camera_url_1 = "http://10.0.0.233:5000/stream"  # Replace with your IP camera URL
+        return Response(routes.gen_ip(webcam1, ip_camera_url_1), mimetype='multipart/x-mixed-replace; boundary=frame')
     except Exception as err:
         return Response(f'Error {err}')
 
