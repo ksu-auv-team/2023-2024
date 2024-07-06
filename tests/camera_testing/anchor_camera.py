951c6684029ae7a038d8c18754963fafdbe95d73
GSS <gurbirsingh2025@gmail.com>
2024-07-06 11:47:41 -0400

    added ip functionality for 2nd camera - tweaked anchor_camera to be able to run in flask - NOTE: still needs to be tested on the Orin

--- tests/camera_testing/anchor_camera.py
+++ tests/camera_testing/anchor_camera.py
@@ -1,22 +1,19 @@
 
 #commented out as this camera is currently inactive and leaving as is causes errors
-'''from flask import Flask, render_template, Response, Blueprint
+from flask import Flask, render_template, Response, Blueprint
 from controllers import routes
 from services.WebCamService import WebCam
 import cv2
 
-camera2_blueprint = Blueprint('anchor_blueprint', __name__)
-
-camera2 = WebCam(camera_number=1)
+anchor_blueprint = Blueprint('anchor_blueprint', __name__)
 
 
-@camera2_blueprint.route('/video_1')
+@anchor_blueprint.route('/video_1')
 def video_1():
     try:
         anchor_camera = WebCam(camera_number=1)
-        #ip_camera_url_1 = "http://10.0.0.233:5000/stream"  # Replace with your IP camera URL
-        return Response(routes.gen(camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(routes.gen(anchor_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
     except Exception as err:
         return Response(f'Error {err}')
-'''
+
 
--- tests/camera_testing/zedcam.py
+++ tests/camera_testing/zedcam.py
@@ -5,14 +5,14 @@ import cv2
 
 zedcam_blueprint = Blueprint('zedcam_blueprint', __name__)
 
-zedcam = WebCam(ip="http://10.101.188.170:5000/stream", camera_number=0)
+#zedcam = WebCam(ip="http://10.101.188.170:5000/stream", camera_number=0)
 
 
 @zedcam_blueprint.route('/video_0')
 def video_0():
     try:
         # Replace with your IP camera URL
-        webcam0 = WebCam(camera_number=0)
-        return Response(routes.gen(webcam0), mimetype='multipart/x-mixed-replace; boundary=frame')
+        zedcam = WebCam(camera_number=0)
+        return Response(routes.gen(zedcam), mimetype='multipart/x-mixed-replace; boundary=frame')
     except Exception as err:
         return Response(f'Error {err}')
