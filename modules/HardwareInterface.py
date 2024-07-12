1664cac0f0c5c9b52578fd8e7f9455addea300c3
Lees <msle237.lees@gmail.com>
2024-07-12 17:34:55 -0400

    hardware interface testing

--- modules/HardwareInterface.py
+++ modules/HardwareInterface.py
@@ -279,7 +279,7 @@ class HardwareInterface:
             print("Error writing I2C data:", str(e))
 
     def write_BatteryMonitor(self, data = [0, 0, 127]):
-        device_address = 0x0A
+        device_address = 0x0a
         try:
             self.bus.write_i2c_block_data(device_address, 0, data)
             # print("Message sent:", data)
