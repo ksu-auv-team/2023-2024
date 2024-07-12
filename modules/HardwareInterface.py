55cc23dd1b053be246e80b537bfe5dd74a0225bf
Lees <msle237.lees@gmail.com>
2024-07-12 19:23:30 -0400

    testing

--- modules/HardwareInterface.py
+++ modules/HardwareInterface.py
@@ -450,7 +450,7 @@ class HardwareInterface:
 
         while True:
             battery_monitor_data = self.read_BatteryMonitor()
-            IMU_data = self.read_IMU()
+            # IMU_data = self.read_IMU()
             # temp_humi_data = self.read_Temp_Humi()
             sensor_data = {
                 "voltage1": battery_monitor_data[0],
@@ -461,12 +461,18 @@ class HardwareInterface:
                 "current3": battery_monitor_data[5],
                 "error": battery_monitor_data[6],
                 "depth": 0,
-                "X": IMU_data["accel_x"],
-                "Y": IMU_data["accel_y"],
-                "Z": IMU_data["accel_z"],
-                "pitch": IMU_data["gyro_x"],
-                "roll": IMU_data["gyro_y"],
-                "yaw": IMU_data["gyro_z"],
+                # "X": IMU_data["accel_x"],
+                # "Y": IMU_data["accel_y"],
+                # "Z": IMU_data["accel_z"],
+                # "pitch": IMU_data["gyro_x"],
+                # "roll": IMU_data["gyro_y"],
+                # "yaw": IMU_data["gyro_z"],
+                "X": 0,
+                "Y": 0,
+                "Z": 0,
+                "pitch": 0,
+                "roll": 0,
+                "yaw": 0,
                 "temperature": 0,
                 "orin_temp": 0,
                 "humidity": 0,
