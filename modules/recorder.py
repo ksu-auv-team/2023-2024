17e2601c1bb64aabee33dfa1736bd012d7ed8a11
Ryan slovensky <ryaneslovensky@yahoo.com>
2024-08-08 17:55:20 -0400

    recorder

--- modules/recorder.py
+++ modules/recorder.py
@@ -14,7 +14,7 @@ class recorder:
         size = (frame_width, frame_height) 
 
         self.result = cv2.VideoWriter(f'path/media/user/7000-8000/AUV/{str(time.time())}-recording{str(self.camera_id)}.mp4',  
-                         cv2.cv.CV_FOURCC(*'H264'), 
+                         cv2.VideoWriter_fourcc(*'MP4V'), 
                          30, size) 
     def run(self):
         time.sleep(10)
