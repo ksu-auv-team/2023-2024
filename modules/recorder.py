c79efea52b8f5d897369ba6d8e6e768df344e4c8
Ryan slovensky <ryaneslovensky@yahoo.com>
2024-08-08 17:57:04 -0400

    movement package

--- modules/recorder.py
+++ modules/recorder.py
@@ -14,7 +14,7 @@ class recorder:
         size = (frame_width, frame_height) 
 
         self.result = cv2.VideoWriter(f'path/media/user/7000-8000/AUV/{str(time.time())}-recording{str(self.camera_id)}.mp4',  
-                         cv2.VideoWriter_fourcc(*'MP4V'), 
+                         cv2.VideoWriter_fourcc(*'MPEG-4'), 
                          30, size) 
     def run(self):
         time.sleep(10)
