d44340a63e2216c2006d8e84e318bb6ff6cbf39f
Lees <msle237.lees@gmail.com>
2024-08-22 12:07:37 -0400

    fixed the package installer

--- src/modules/SupportAll/PackageInstaller.py
+++ src/modules/SupportAll/PackageInstaller.py
@@ -5,19 +5,25 @@ class PackageInstaller:
     def __init__(self, install: bool = False, debug: bool = False):
         self.install = install
         self.debug = debug
-        
-        if os.path(os.path.join(os.getcwd(), 'requirements.txt')):
+
+        # Check if 'requirements.txt' exists in the current working directory
+        if os.path.exists(os.path.join(os.getcwd(), 'requirements.txt')):
             self.requirements = os.path.join(os.getcwd(), 'requirements.txt')
+        else:
+            self.requirements = None  # Set to None if the file doesn't exist
         
     def installPackages(self):
         if self.install:
             try:
                 if self.requirements:
                     subprocess.run(['pip3', 'install', '-r', self.requirements])
+                else:
+                    print("requirements.txt not found.")
             except Exception as e:
                 print(e)
                 return False
         return True
 
-                
-        
+if __name__ == '__main__':
+    installer = PackageInstaller(install=True, debug=True)
+    installer.installPackages()
